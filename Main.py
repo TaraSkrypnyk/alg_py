e914d2c063ed82bfb40cbc0d52ca5dfe3ddf9c7e
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-13 23:38:36 +0200

    Ex2 was done

--- Main.py
+++ Main.py
@@ -1,4 +1,8 @@
 try:
-
+    print('"Anyone who')
+    print("stops")
+    print("learning is old,")
+    print('whether at twenty or eighty"')
+    print("Henry Ford")
 except Exception as e:
     print(e)
